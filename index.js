5da03a968b2b9819fdb67f0a9fc4a6cd6ea7c5fa
Sandra G <sandyjtech@gmail.com>
2023-06-12 07:02:43 -0700

    added function

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(answer = "roller-skate") {
+  return `This Saturday, I want to ${answer}!`;
+}
+saturdayFun();
+function mondayWork(answer = "go to the office") {
+  return `This Monday, I will ${answer}.`;
+}
+mondayWork();
+
+function wrapAdjective(flair = "*") {
+  return function (adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
+
+const encouragingPromptFunction = wrapAdjective("||");
